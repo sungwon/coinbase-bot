4aa4742ae2f8bf4ef9e50bae910ac645dee1ce29
sungwon <dokkaebi@gmail.com>
2014-11-12 08:53:32 -0500

    fix logic bug and added safety check

--- coinbasebot.rb
+++ coinbasebot.rb
@@ -5,14 +5,14 @@ require 'mail' # gem 'mail'
 BUY_CEIL = Money.new(45000, "USD")
 SELL_FLOOR = Money.new(38262, "USD")
 AUTO_BUY = false
-AUTO_SELL = true
+AUTO_SELL = false
 BUY_ALERT = false
 SELL_ALERT = false
 BUY_QUANTITY = 1
 SELL_QUANTITY = 0.5000142
 
-EMAIL_ALERT_ADDRESS = 'username@email.com' # your email address here
-MAIL_SERVER = 'mail.domain.com' # your mail server here
+EMAIL_ALERT_ADDRESS = 'dokkaebi@gmail.com' # your email address here
+MAIL_SERVER = 'mail.sungwonchoe.com' # your mail server here
 SLEEP_SECONDS = 60 # seconds between API calls
 
 I18n.enforce_available_locales = false
@@ -57,20 +57,30 @@ end
 
 puts "Buy ceiling is at: " + BUY_CEIL.format
 puts "Sell floor is at : " + SELL_FLOOR.format
-if (AUTO_BUY) 
+if AUTO_BUY 
   puts "Auto-buy is      : ON"
   puts "Buy quantity is  : #{BUY_QUANTITY}" 
 else
   puts "Auto-buy is      : OFF"
 end
-if (AUTO_SELL) 
+if AUTO_SELL 
   puts "Auto-sell is     : ON"
   puts "Sell quanity is  : #{SELL_QUANTITY}"
 else
   puts "Auto-sell is     : OFF"
 end
 
-loop do
+continue = false
+input = ""
+puts "To proceed press 'y' to quit press 'q' and then 'enter'"
+while (input != "y" && input != "q")
+  input = gets.chomp
+  if (input == "y") 
+    continue = true
+  end
+end
+
+while continue
 
   begin
     buy_price = coinbase.buy_price(1)
@@ -95,7 +105,9 @@ loop do
       send_notice(email_address, "Low buy price ($#{buy_price})", "Low buy price ($#{buy_price})")
       break
     end
-  elsif sell_price >= SELL_FLOOR
+  end
+  
+  if sell_price >= SELL_FLOOR
     if AUTO_SELL
       logger.info "Sell Price ($#{sell_price}) above target ($#{SELL_FLOOR}). Selling automatically."
       coinbase.sell!(SELL_QUANTITY);
@@ -107,5 +119,6 @@ loop do
       break
     end
   end
+  
   sleep SLEEP_SECONDS
 end
